766a316682bf5dcf98aa7e017d5af7b971fa3e9c
JoaoPedroCardoso <joaopedrocar@hotmail.com>
2019-01-08 16:50:35 -0200

    Adding swaggerw

--- src/main/java/com/poc/kafka/controller/KafkaController.java
+++ src/main/java/com/poc/kafka/controller/KafkaController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value= "/kafka")
+@RequestMapping(value = "/api/kafka")
 public class KafkaController {
 
     @Autowired
